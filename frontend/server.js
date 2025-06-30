a9419d15feb105a4a5337d98fcc2393597543d10
Ramses Rojas <rojas.aguilar.ramses721@outlook.com>
2025-06-30 12:43:04 -0700

    profile and up book completed

--- frontend/server.js
+++ frontend/server.js
@@ -1,26 +1,38 @@
-const http = require('http');
+const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
 const port = 3000;
-const data  = fs.readFileSync(`index.html`, 'utf-8');
-const server = http.createServer((req,res) => {
-    if(req.url === '/'){
-        // const filePath = path.join(__dirname,'index.html');
-        // fs.readFileSync(filePath, (err,data) => {
-        //     if(err) {
-        //         res.writeHead(500);
-        //         return res.end('Error');
-        //     }
-        //     res.writeHead(200,{'Content-Type' : 'text/html'});
-        //     console.log("served")
-        //     return res.end(data);
+let index = fs.readFileSync(`index.html`, "utf-8");
+const cat = fs.readFileSync(`${__dirname}/assets/cat.jpg`);
+const style = fs.readFileSync("style.css", "utf-8");
 
-       res.writeHead(200,{'Content-Type' : 'text/html'});
-        res.end(data)
-        };
-})
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(index);
+  }
 
-server.listen(port,'127.0.0.1', () => {
-    console.log(`listening in ${port}`)
-})
+  if (req.url === "/style.css") res.end(style);
+
+  if (req.url === "/assets/cat.jpg") {
+    res.writeHead(200, { "content-type": "image/jpeg" });
+    res.end(cat);
+  }
+
+  if (req.url === "/perfil") {
+    const perfil = fs.readFileSync(`${__dirname}/pages/template-profile.html`);
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end(index.replace('<div id="main-page"></div>', perfil));
+  }
+
+  if (req.url === "/agregar") {
+    const perfil = fs.readFileSync(`${__dirname}/pages/template-subir.html`);
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end(index.replace('<div id="main-page"></div>', perfil));
+  }
+});
+
+server.listen(port, "127.0.0.1", () => {
+  console.log(`listening in ${port}`);
+});
