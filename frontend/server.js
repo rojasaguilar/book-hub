c87d90b37825e884c2f47870e6933fa62eaccb77
Ramses Rojas <rojas.aguilar.ramses721@gmail.com>
2025-06-30 23:29:33 -0700

    prettier

--- frontend/server.js
+++ frontend/server.js
@@ -1,16 +1,16 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const mongoose = require('mongoose')
-const User = require('./models/users')
+const mongoose = require("mongoose");
+const User = require("./models/users");
 
-const connectDB = require('./database')
+const connectDB = require("./database");
 
 connectDB();
 const port = 3000;
 let index = fs.readFileSync(`index.html`, "utf-8");
 const cat = fs.readFileSync(`${__dirname}/assets/cat.jpg`);
-const icon = fs.readFileSync(`${__dirname}/assets/book-hub.png`)
+const icon = fs.readFileSync(`${__dirname}/assets/book-hub.png`);
 const style = fs.readFileSync("style.css", "utf-8");
 
 const server = http.createServer((req, res) => {
@@ -26,24 +26,24 @@ const server = http.createServer((req, res) => {
     res.end(cat);
   }
 
-    if (req.url === "/assets/book-hub.png") {
+  if (req.url === "/assets/book-hub.png") {
     res.writeHead(200, { "content-type": "image/png" });
     res.end(icon);
   }
 
-  if(req.url.includes('/assets/icons')) {
-    let icon = fs.readFileSync(`${__dirname}${req.url}`)
+  if (req.url.includes("/assets/icons")) {
+    let icon = fs.readFileSync(`${__dirname}${req.url}`);
     res.writeHead(200, { "content-type": "image/svg+xml" });
     res.end(icon);
   }
 
-  if(req.url === "/find"){
- getUsers()
- .then(usuarios => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(usuarios))
- })
- .catch(err => res.end(`${err}`))
+  if (req.url === "/find") {
+    getUsers()
+      .then((usuarios) => {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(usuarios));
+      })
+      .catch((err) => res.end(`${err}`));
   }
 
   if (req.url === "/perfil") {
@@ -59,15 +59,14 @@ const server = http.createServer((req, res) => {
   }
 });
 
-mongoose.connection.once('open', () => {
-  console.log("connected to mongodb")
+mongoose.connection.once("open", () => {
+  console.log("connected to mongodb");
   server.listen(port, "127.0.0.1", () => {
-  console.log(`listening in ${port}`);
+    console.log(`listening in ${port}`);
+  });
 });
-})
 
 const getUsers = async () => {
   const usuarios = await User.find();
   return usuarios;
-}
-
+};
